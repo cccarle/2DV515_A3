360c4565931fff07bdce70fc8a935ac7e6daadff
cccarle <carleinarsson@live.se>
2019-12-05 12:56:31 +0100

    added the normalize method to get the scores right

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -27,20 +27,40 @@ const getAllPagesThatIncludeWord = (word, allPages) => {
 
     page.words.forEach(wordInPage => {
       if (word == wordInPage) {
-        score++
+        score += 1
         page.score = score
         pagesWithTheWord.push(page)
       }
     })
   })
 
+  normalize(pagesWithTheWord, getMaxValue(pagesWithTheWord))
+
   return pagesWithTheWord
     .map(page => {
-      return { url: page.url, score: page.score }
+      return { url: page.url, score: page.normalizedScore }
     })
     .filter(
       (value, index, self) => self.map(x => x.url).indexOf(value.url) == index
     )
+}
+
+/* 
+Returns the max value of the scores.
+*/
+
+const getMaxValue = pagesWithTheWord => {
+  return Math.max(...pagesWithTheWord.map(page => page.score))
+}
+
+/* 
+Normalize the score for every pages score by dividing it by the max value of the scores.
+*/
+
+const normalize = (pagesWithTheWord, max) => {
+  pagesWithTheWord.map(
+    page => (page.normalizedScore = (page.score / max).toFixed(2))
+  )
 }
 
 module.exports = {
